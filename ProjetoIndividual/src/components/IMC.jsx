19692049a6090759f36318198bf18b46f6452a96
João Guilherme Piccoli <joaoguilhermepiccoli@gmail.com>
2024-11-01 15:44:56 -0300

    correção de erros

--- ProjetoIndividual/src/components/IMC.jsx
+++ ProjetoIndividual/src/components/IMC.jsx
@@ -4,7 +4,6 @@ export default function IMC() {
   const [nome, setNome] = useState("");
   const [peso, setPeso] = useState(0.0);
   const [altura, setAltura] = useState(0.0);
-  const [texto, setTexto] = useState("");
   const [resposta, setResposta] = useState("");
 
   function CalcularImc(event) {
@@ -14,25 +13,25 @@ export default function IMC() {
     const imc = peso / (calcAltura * calcAltura);
     let resultado = "";
 
-    if (imc < 18.5) {
-      resultado = "baixo peso";
-    } else if (imc < 24.9) {
-      resultado = "peso ideal";
-    } else if (imc < 29.9) {
-      resultado = "sobrepeso";
-    } else if (imc < 34.9) {
-      resultado = "obesidade de grau 1";
-    } else if (imc < 39.9) {
-      resultado = "obesidade de grau 2";
-    } else {
-      resultado = "obesidade extrema";
-    }
-    setTexto(resultado);
+      if (imc < 18.5) {
+        resultado = "baixo peso";
+      } else if (imc < 24.9) {
+        resultado = "peso ideal";
+      } else if (imc < 29.9) {
+        resultado = "sobrepeso";
+      } else if (imc < 34.9) {
+        resultado = "obesidade de grau 1";
+      } else if (imc < 39.9) {
+        resultado = "obesidade de grau 2";
+      } else {
+        resultado = "obesidade extrema";
+      }
+
     setResposta(
-      `${nome} com a sua altura de ${altura} cm e seu peso de ${peso}Kg você está com ${texto}`
+      `${nome} com a sua altura de ${altura} cm e seu peso de ${peso}Kg você está com ${resultado}`
     );
     console.log(
-      `Com a sua altura de ${altura} cm e seu peso de ${peso}Kg você está com ${texto}`
+      `Com a sua altura de ${altura} cm e seu peso de ${peso}Kg você está com ${resultado}`
     );
   }
 
@@ -59,15 +58,15 @@ export default function IMC() {
           <label>Altura: </label>
           <input
             type="number"
-            placeholder="Digite a sua Altura"
+            placeholder="Digite a sua Altura em cm"
             name="altura"
             onChange={(e) => setAltura(parseFloat(e.target.value))}
           />
           <hr />
-          <button type="submit" onClick={resposta}>
+          <button type="submit">
             Calcular IMC
           </button>
-          {resposta && <p>{resposta}</p>}
+          <p>{resposta}</p>
         </form>
       </div>
     </div>
